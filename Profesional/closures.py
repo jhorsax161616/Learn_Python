12302cd464fb89596a5e322d2f2d62f33456f74c
jhorsax <jhorsax161616@gmail.com>
2021-10-21 08:53:35 -0500

    Clousures

--- Profesional/closures.py
+++ Profesional/closures.py
@@ -1,13 +1,33 @@
-def make_multiplier(x):
+#Hola 3 -> HolaHolaHola
 
-    def multiplier(n):
-        return x * n
-    
-    return multiplier
+def make_repeater_of(n: int):
+    def repeater(string: str) -> str:
+        assert type(string) == str, "Solo se debe recibir cadenas"
+        return string * n
+    return repeater
 
-times10 = make_multiplier(10)
-times4 = make_multiplier(4)
+def make_division_by(n: float):
+    assert n != 0, "No se acepta factor de divisor 0!!!"
+    def division(number: float) -> float:
+        return number/n
+    return division
 
-print(times10(4))
-print(times4(3))
-print(times4(times10(2)))
+def run() -> None:
+    repeat3 = make_repeater_of(3)
+    repeat4 = make_repeater_of(4)
+    repeat8 = make_repeater_of(8)
+
+    print(repeat3("Hola"))
+    print(repeat4("Power"))
+    print(repeat8("Sa"))
+
+    division_by_3 = make_division_by(3)
+    division_by_5 = make_division_by(5)
+    division_by_18 = make_division_by(18)
+
+    print(division_by_3(18))
+    print(division_by_5(100))
+    print(division_by_18(54))
+   
+if __name__ == '__main__':
+    run()
